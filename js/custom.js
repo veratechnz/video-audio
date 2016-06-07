a2b809105dbef570d37f903eb3e4487dbfe7ab46
veratech <jared@veratech.co.nz>
2016-06-07 16:14:13 +1200

    latest-new

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,27 @@
-		var vid = document.getElementById("bgvid"),
-		pauseButton = document.getElementById("vidpause");
-		function vidFade() {
-		    vid.classList.add("stopfade");
-		}
 
-		vid.addEventListener('ended', function() {
-		    // only functional if "loop" is removed 
-		     vid.pause();
-			// to capture IE10
-			vidFade();
-		});
-
-		pauseButton.addEventListener("click", function() {
-		    vid.classList.toggle("stopfade");
-			if (vid.paused) {
-		vid.play();
-				pauseButton.innerHTML = "Pause";
-			} else {
-		        vid.pause();
-		        pauseButton.innerHTML = "Paused";
+// Original fullscreen function for playback etc
+var fullScreen = function(){
+	var vid = document.getElementById("bgvid"),
+			pauseButton = document.getElementById("vidpause");
+			function vidFade() {
+			    vid.classList.add("stopfade");
 			}
-		});
+
+			vid.addEventListener('ended', function() {
+			    // only functional if "loop" is removed 
+			     vid.pause();
+				// to capture IE10
+				vidFade();
+			});
+
+			pauseButton.addEventListener("click", function() {
+			    vid.classList.toggle("stopfade");
+				if (vid.paused) {
+			vid.play();
+					pauseButton.innerHTML = "Pause";
+				} else {
+			        vid.pause();
+			        pauseButton.innerHTML = "Paused";
+				}
+			});
+}();
